666a480ac0d51519e7ca82090bcf6a05e9556d34
Achref <medachrefg@gmail.com>
2022-03-22 00:24:02 +0100

    cleaning the code and make all pages use shared.js too

--- lesson1/shared.js
+++ lesson1/shared.js
@@ -3,7 +3,6 @@ var modal = document.querySelector('.modal');
 // console.dir(backdrop);
 var modalNoButton = document.querySelector('.modal__action--negative');
 var selectBtns = document.querySelectorAll('.plan button');
-
 var toggleButton = document.querySelector('.toggle-button');
 var mobileNav = document.querySelector('.mobile-nav');
 
@@ -17,11 +16,15 @@ backdrop.addEventListener('click', () => {
 	mobileNav.style.display = 'none';
 	closeModal();
 });
-modalNoButton.addEventListener('click', closeModal);
+if (modalNoButton) {
+	modalNoButton.addEventListener('click', closeModal);
+}
 
 function closeModal() {
+	if (modal) {
+		modal.style.display = 'nome';
+	}
 	backdrop.style.display = 'none';
-	modal.style.display = 'nome';
 }
 
 toggleButton.addEventListener('click', () => {
