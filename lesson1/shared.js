b31540b338bef2c685083b9a9d49f2485c0a129e
Achref <medachrefg@gmail.com>
2022-03-21 23:47:23 +0100

    opening and closing the hamburger menu

--- lesson1/shared.js
+++ lesson1/shared.js
@@ -1,10 +1,30 @@
 var backdrop = document.querySelector('.backdrop');
-// console.dir(backdrop);
-var selectBtns = document.querySelectorAll('.plan button');
 var modal = document.querySelector('.modal');
+// console.dir(backdrop);
+var modalNoButton = document.querySelector('.modal__action--negative');
+var selectBtns = document.querySelectorAll('.plan button');
+
+var toggleButton = document.querySelector('.toggle-button');
+var mobileNav = document.querySelector('.mobile-nav');
+
 for (const btn of selectBtns) {
-	btn.addEventListener('click', (event) => {
+	btn.addEventListener('click', () => {
 		modal.style.display = 'block';
 		backdrop.style.display = 'block';
 	});
 }
+backdrop.addEventListener('click', () => {
+	mobileNav.style.display = 'none';
+	closeModal();
+});
+modalNoButton.addEventListener('click', closeModal);
+
+function closeModal() {
+	backdrop.style.display = 'none';
+	modal.style.display = 'nome';
+}
+
+toggleButton.addEventListener('click', () => {
+	mobileNav.style.display = 'block';
+	backdrop.style.display = 'block';
+});
